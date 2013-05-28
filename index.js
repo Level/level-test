98b0f440b0c9ce08e2f6600b62f9b9adeb705bbe
Dominic Tarr <dominic.tarr@gmail.com>
2013-05-28 17:54:55 +0200

    work in 0.8

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ var rimraf = require('rimraf')
 var os = require('os')
 var path = require('path')
 
+function tmpdir () {
+  return os.tmpdir ? os.tmpdir() : process.env.TMPDIR
+}
+
 module.exports = function () {
 
   return function (name, opts, cb) {
     name = name || 'db_' + Date.now()
-    var dir = path.join(os.tmpdir(), name)
+    var dir = path.join(tmpdir(), name)
     rimraf.sync(dir)
     return level(dir, opts, cb)
   }
