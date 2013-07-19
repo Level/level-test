8718bc1b4293c4e26749be69e5632c7c8edf0762
Dominic Tarr <dominic.tarr@gmail.com>
2013-07-18 19:27:56 -0700

    use tmpdir module

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var levelup = require('levelup')
 var Memdown = require('memdown') 
 var rimraf = require('rimraf')
-var os = require('os')
+var tmpdir = require('tmpdir')
 var path = require('path')
-
-function tmpdir () {
-  return os.tmpdir ? os.tmpdir() : process.env.TMPDIR
-}
 
 function disk (opts) {
   return function (name, _opts, cb) {
     name = name || 'db_' + Date.now()
-    var dir = path.join(tmpdir(), name)
+    var dir = path.join(tmpdir, name)
     if(opts.clean !== false)
       rimraf.sync(dir)
 
