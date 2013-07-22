9745fe4e873ccc8d41feacffcd056ab80151944f
Julian Gruber <julian@juliangruber.com>
2013-07-22 21:38:11 +0200

    added mem-option to browser version

--- browser.js
+++ browser.js
@@ -1,15 +1,25 @@
 var levelup = require('levelup')
 var Leveljs = require('level-js')
+var Memdown = require('memdown')
 
-function browser (name, _opts, cb) {
+function browser (opts) {
+  return function (name, _opts, cb) {
+    name = name || 'db_' + Date.now()
+    _opts = _opts || {}
+    _opts.db = function (l) { return new Leveljs(l) }
+    return levelup(name, _opts, cb)
+  }
+}
+
+function mem (name, _opts, cb) {
   _opts = _opts || {}
-  _opts.db = function (l) { return new Leveljs(l) }
-  name = name || 'level-js' + Date.now() + Math.random()
+  _opts.db = function (l) { return new Memdown(l) }
+  name = name || 'in-memory'
   return levelup(name, _opts, cb)
 }
 
 module.exports = function (opts) {
   opts = opts || {}
-  return browser 
+  return opts.mem ? mem : browser(opts)
 }
 
