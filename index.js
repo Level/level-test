815b74c56210a150ecca5d21b8e3e84cdb22ed97
Bryce Baril <bryce@ravenwall.com>
2014-01-29 15:49:27 -0800

    Fix options clobber for creating new leveldb instance.

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var Memdown = require('memdown')
 var rimraf = require('rimraf')
 var tmpdir = require('osenv').tmpdir()
 var path = require('path')
+var xtend = require('xtend')
 function disk (opts) {
   var leveldown = require('leveldown')
   return function (name, _opts, cb) {
-    _opts = opts || {}
+    _opts = xtend(opts, _opts)
     name = name || 'db_' + Date.now()
     var dir = path.join(tmpdir, name)
     if(opts.clean !== false)
