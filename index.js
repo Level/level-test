60b3fc39ea879e332dcfe05d7f5280221eda2f4a
Dominic Tarr <dominic.tarr@gmail.com>
2014-05-13 22:18:26 +0200

    make leveldown optional, for browser testing.

--- index.js
+++ index.js
@@ -4,8 +4,17 @@ var rimraf = require('rimraf')
 var tmpdir = require('osenv').tmpdir()
 var path = require('path')
 var xtend = require('xtend')
+function maybeLevelDown () {
+  try {
+    return require('leveldown')
+  } catch (err) {
+    console.error('could not load leveldown, fallback to memdown')
+    return Memdown
+  }
+}
+
 function disk (opts) {
-  var leveldown = require('leveldown')
+  var leveldown = maybeLevelDown()
   return function (name, _opts, cb) {
     _opts = xtend(opts, _opts)
     name = name || 'db_' + Date.now()
