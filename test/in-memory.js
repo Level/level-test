4c0ec0d06a8bffb972e560f1b7fea57c6e36a206
Vincent Weevers <mail@vincentweevers.nl>
2018-05-03 22:38:51 +0200

    test: db is backed by memdown

--- test/in-memory.js
+++ test/in-memory.js
@@ -1,10 +1,17 @@
 var level = require('../')({ mem: true })
+var memdown = require('memdown')
 var test = require('tape')
 
 test('simple', function (t) {
+  t.plan(4)
+
   var db = level('level-test-simple-mem', {encoding: 'json'})
   var key = '' + Math.random()
   var value = '' + new Date()
+
+  db.on('open', function () {
+    t.ok(innerDb(db) instanceof memdown, 'backed by memdown')
+  })
 
   db.put(key, value, function (err) {
     t.notOk(err)
@@ -12,16 +19,26 @@ test('simple', function (t) {
     db.get(key, function (err, _value) {
       t.ifError(err)
       t.equal(_value, value)
-      t.end()
     })
   })
 })
 
 test('default name', function (t) {
+  t.plan(3)
+
   var db = level()
+
+  db.on('open', function () {
+    t.ok(innerDb(db) instanceof memdown, 'backed by memdown')
+  })
+
   db.put('foo', 'bar', function (err) {
     t.ifError(err)
     t.notOk(err)
-    t.end()
   })
 })
+
+function innerDb (db) {
+  while (db.db) db = db.db
+  return db
+}
