be943de8dbb093cbdc0e3f4b622b406644317261
Vincent Weevers <mail@vincentweevers.nl>
2018-05-04 20:05:55 +0200

    dedup code between node and browser

--- index.js
+++ index.js
@@ -1,63 +1,29 @@
 'use strict'
 
-var packager = require('level-packager')
 var memdown = require('memdown')
 var rimraf = require('rimraf')
 var mkdirp = require('mkdirp')
 var tmpdir = require('osenv').tmpdir()
 var path = require('path')
-var xtend = require('xtend')
+var wrap = require('./wrap')
 
-function getDown (opts) {
+function getDown (defaults) {
   try {
     return require('leveldown')
   } catch (err) {
     console.error('could not require leveldown, fallback to memdown')
-
-    opts.mem = true
+    defaults.mem = true
     return memdown
   }
 }
 
-function wrap (down, parentOpts) {
-  var levelup = packager(down)
-
-  return function (loc, opts, cb) {
-    if (typeof loc === 'function') { // (cb)
-      cb = loc
-      loc = null
-      opts = null
-    } else if (typeof loc === 'object' && loc !== null) { // (opts[, cb])
-      cb = opts
-      opts = loc
-      loc = null
-    } else if (typeof opts === 'function') { // (name, cb)
-      cb = opts
-      opts = null
-    }
-
-    opts = xtend(parentOpts, opts)
-
-    if (!parentOpts.mem) {
-      loc = loc || 'db_' + Date.now()
-      mkdirp.sync(tmpdir)
-      loc = path.join(tmpdir, loc)
-      if (opts.clean !== false) rimraf.sync(loc)
-    }
-
-    // Note: memdown ignores loc
-    return levelup(loc, opts, cb)
-  }
+function getLocation (name) {
+  mkdirp.sync(tmpdir)
+  return path.join(tmpdir, name)
 }
 
-module.exports = function (down, opts) {
-  if (typeof down !== 'function') {
-    opts = down
-    down = null
-  }
-
-  opts = xtend(opts)
-  down = down || (opts.mem ? memdown : getDown(opts))
-
-  return wrap(down, opts)
+function clean (location) {
+  rimraf.sync(location)
 }
+
+module.exports = wrap(getDown, getLocation, clean)
