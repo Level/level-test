51306131f95bc95d0a233931a3c06be3685ff795
Vincent Weevers <mail@vincentweevers.nl>
2019-05-25 14:53:34 +0300

    Refactor (#116)
    - Replace level-packager with level-compose
    - Experimental: support overriding the layers (encoding-down and levelup)
    - Experimental: support passing in a location
    - Remove defunct clean option
    - Remove memdown fallback in Node.js

--- test/node.js
+++ test/node.js
@@ -3,6 +3,8 @@
 var leveldown = require('leveldown')
 var memdown = require('memdown')
 var jsondown = require('jsondown')
+var encode = require('encoding-down')
+var levelup = require('levelup')
 var levelTest = require('../')
 var suite = require('./suite')
 
@@ -13,5 +15,12 @@ suite.args(levelTest({ mem: true }), memdown)
 suite.args(levelTest(memdown, { mem: true }), memdown)
 suite.args(levelTest(jsondown), jsondown)
 
-suite.clean(levelTest())
+// Without encoding-down or levelup
+suite.customLayers(levelTest({ mem: true, layers: [] }), memdown, memdown, 'value')
+suite.customLayers(levelTest(leveldown, { layers: [] }), leveldown, leveldown, 'value')
+suite.customLayers(levelTest({ layers: [encode] }), leveldown, encode, 'value')
+suite.customLayers(levelTest({ layers: [encode, levelup] }), leveldown, levelup, 'value')
+suite.customLayers(levelTest(memdown, { layers: [levelup], mem: true }), memdown, levelup, 'value')
+suite.customLayers(levelTest(memdown, { layers: [encode, { valueEncoding: 'json' }] }), memdown, encode, '"value"')
+
 suite.options(levelTest)
